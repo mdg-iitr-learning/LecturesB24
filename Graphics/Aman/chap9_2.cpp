74e499f816bddc1da2de0f8c5eee57a9464e2da5
aman-singh7 <aman2@me.iitr.ac.in>
2021-04-26 00:05:11 +0530

    Chapter-09

---
+++ Graphics/Aman/chap9_2.cpp
@@ -0,0 +1,166 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
+#include "camera.h"
+#define drand48() ((double) rand()/RAND_MAX)
+
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p=2.0*vec3(drand48(), drand48(), drand48()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+vec3 color(const ray& r, hitable *world, int depth){
+    hit_record rec;
+    if(world -> hit(r,0.001, FLT_MAX, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if(depth < 50 && rec.mat_ptr -> scatter(r,rec,attenuation,scattered)){
+            return attenuation*color(scattered, world, depth+1);
+        }
+        else {
+            return vec3(0,0,0);
+        }
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t= 0.5*(unit_direction.y() +1.0);
+        return (1.0 -t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+    }
+}
+
+class lambertain: public material {
+    public:
+        lambertain (const vec3& a) : albedo(a) {}
+        virtual bool scatter( const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+            scattered = ray(rec.p, target-rec.p);
+            attenuation = albedo;
+            return true;
+        }
+        vec3 albedo;
+};
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v-2*dot(v,n)*n;
+}
+
+class metal : public material {
+    public:
+        metal(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& r_in,const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal)>0);
+        }
+        vec3 albedo;
+        float fuzz;
+};
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt,vec3& refracated){
+    vec3 uv= unit_vector(v);
+    float dt = dot(uv,n);
+    float discriminant = 1.0 -(ni_over_nt*ni_over_nt)*(1-dt*dt);
+    if(discriminant >0){
+        refracated = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
+        return true;
+    }
+    else 
+        return false;
+}
+
+float schlick (float cosine, float ref_idx){
+    float r0 = (1-ref_idx) / (1 + ref_idx);
+    r0*=r0;
+    return r0 + (1- r0)*pow((1-cosine),5);
+}
+
+class dielectric : public material {
+    public:
+        dielectric(float ri) : ref_idx(ri) {}
+        virtual bool scatter( const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0,1.0,1.0);
+            vec3 refracted;
+            float reflect_prob;
+            float cosine;
+            if(dot(r_in.direction(), rec.normal)>0){
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+                cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+            else{
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0/ref_idx;
+                cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+            if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+                reflect_prob = schlick(cosine, ref_idx);
+            }
+            else{
+                scattered = ray(rec.p, reflected);
+                reflect_prob = 1.0;
+            }
+            if( drand48() < reflect_prob){
+                scattered = ray(rec.p, reflected);
+            }
+            else{
+                scattered = ray(rec.p, refracted);
+            }
+            return true;
+        }
+        float ref_idx;
+};
+
+
+int main(){
+    std::ofstream fout("chap9_2.ppm");
+    if(fout.fail()) return -1;
+
+    int nx=200;
+    int ny=100;
+    int ns=100;
+    fout<<"P3\n"<<nx<<" "<<ny<<"\n255\n";
+    static lambertain diffuse_grey(vec3(0.1,0.2,0.5));
+    static lambertain diffuse_grey1(vec3(0.8,0.8,0.0));
+    static metal reddish_metal(vec3(0.8,0.6,0.2));
+    static metal grey_metal(vec3(0.8,0.8,0.8));
+    hitable *list[5];
+    list[0] = new sphere(vec3(0,0,-1),0.5,&diffuse_grey);
+    list[1] = new sphere(vec3(0,-100.5,-1),100, &diffuse_grey1);
+    list[2] = new sphere(vec3(1,0,-1),0.5, &reddish_metal);
+    list[3] = new sphere(vec3(-1,0,-1), 0.5,new dielectric(1.5));
+    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    hitable *world = new hitable_list(list,5);
+    camera cam;
+    for(int j=ny-1;j>=0;j--){
+        for(int i=0;i<nx;i++){
+            vec3 col(0,0,0);
+            for(int s=0;s<ns;s++){
+                float u = float(i + drand48())/ float(nx);
+                float v= float(j+ drand48()) / float(ny);
+                ray r = cam.get_ray(u,v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r,world,0);
+            }
+            col/= float(ns);
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+
+            fout<<ir<<" "<<ig<<" "<<ib<<"\n";
+        }
+    }
+    std::cout<<"Successful\n";
+    return 0;
+}
