5122deaa79f1b6cc26caec5635ec5e3e8723e273
Saumya <saumya_t@cs.iitr.ac.in>
2021-04-29 21:56:46 +0530

    CHAPTER 5 (ONE SPHERE)
    -Implemented Chapter 5 with just one sphere

--- Graphics/Saumya_Talera/main.cpp
+++ Graphics/Saumya_Talera/main.cpp
@@ -13,21 +13,30 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
   if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin()-center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0*dot(oc, r.direction());
     float c = dot(oc,oc)-radius*radius;
     float discriminant = b*b-4*a*c;
-    return (discriminant>0);
+    if(discriminant<0.0){
+        return (-1.0);
+    }
+    else {
+        return (-b-sqrt(discriminant))/(2.0*a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if(hit_sphere(vec3(0,0,-1), 0.5, r))
-        return vec3(1,0,0);
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    cout<<t<<endl;
+    if(t>0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t)-vec3(0,0,-1));
+        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5*(unit_direction.y()+1.0);
-    return (1-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+    t = 0.5*(unit_direction.y()+1.0);
+    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
 }
 
 int main() {
@@ -55,6 +64,6 @@ int main() {
             cout<<ir<<" "<<ig<<" "<<ib<<"\n";
         }
     }
-    encodeOneStep("Chapter4.png", image, nx, ny);
+    encodeOneStep("Chapter5.png", image, nx, ny);
 }
 
