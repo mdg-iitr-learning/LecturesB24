c1ee3af4fa07bff4685c9f4187fa85fe37a2d42f
Saumya <saumya_t@cs.iitr.ac.in>
2021-04-30 00:37:35 +0530

    CHAPTER 7
    - Implemented Chapter 7

--- Graphics/Saumya_Talera/main.cpp
+++ Graphics/Saumya_Talera/main.cpp
@@ -16,20 +16,28 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
   if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
+float drand() {
+    return (float)rand()/(RAND_MAX+1.0);
+}
+vec3 random_in_unit_sphere(){
+    vec3 p;
+    do {
+        p = 2.0*vec3(drand(),drand(),drand()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
-    if(world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if(world->hit(r, 0.001, FLT_MAX, rec)) {
+        vec3 target = rec.normal + random_in_unit_sphere();
+        return 0.5*color(ray(rec.p,target),world);
     }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y()+1.0);
     return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
 }
 
-float drand() {
-    return (float)rand()/(RAND_MAX+1.0);
-}
 
 int main() {
     int nx=200;
@@ -47,13 +55,14 @@ int main() {
         for(int i=0;i<nx;i++) {
             vec3 col(0,0,0);
             for(int s=0;s<ns;s++) {
-                float u = ((float)i+(float)rand()/(RAND_MAX+1.0))/(float)nx;
-                float v = ((float)j+(float)rand()/(RAND_MAX+1.0))/(float)ny;
+                float u = ((float)i+drand())/(float)nx;
+                float v = ((float)j+drand())/(float)ny;
                 ray r = cam.get_ray(u,v);
                 vec3 p =r.point_at_parameter(2.0);
                 col += color(r,world);
             }
             col /= (float)ns;
+            col = vec3(sqrt(col[0]),sqrt(col[1]),sqrt(col[2]));
             int ir = (int)(255.99*col[0]);
             int ig = (int)(255.99*col[1]);
             int ib = (int)(255.99*col[2]);
@@ -63,6 +72,6 @@ int main() {
             cout<<ir<<" "<<ig<<" "<<ib<<"\n";
         }
     }
-    encodeOneStep("Chapter6.png", image, nx, ny);
+    encodeOneStep("Chapter7.png", image, nx, ny);
 }
 
