fcd9d9dc4ab187ff6d82685d464bb69d3cb6ed34
Saumya <saumya_t@cs.iitr.ac.in>
2021-04-29 19:47:47 +0530

    CHAPTER 3 AND LODEPNG
    - Implemented chapter 3
    - Added lodepng functionality
    
    From now, each chapter will have the code in main file, a corresponding ppm file and a corresponding png file

--- Graphics/Saumya_Talera/main.cpp
+++ Graphics/Saumya_Talera/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "vec3.h"
+#include "ray.h"
+#include "lodepng.h"
 using namespace std;
+
+void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+    LodePNGColorType colorType = LCT_RGB;
+  //Encode the image
+  unsigned error = lodepng::encode(filename, image, width, height,colorType);
+
+  //if there's an error, display it
+  if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+}
+
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y()+1.0);
+    return (1-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+}
 
 int main() {
     int nx=200;
     int ny=100;
+    vector<unsigned char> image;
+    image.resize(nx*ny*3);
     cout<<"P3\n"<<nx<<" "<<ny<<"\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0 ,0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
     for(int j=ny-1;j>=0;j--) {
         for(int i=0;i<nx;i++) {
-            vec3 col((float)i/(float)nx,(float)j/(float)ny,0.2f);
+            float u = (float)i/(float)nx;
+            float v = (float)j/(float)ny;
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
             int ir = (int)(255.99*col[0]);
             int ig = (int)(255.99*col[1]);
             int ib = (int)(255.99*col[2]);
+            image[3*nx*(ny-1-j)+3*i+0] = (char)ir;
+            image[3*nx*(ny-1-j)+3*i+1] = (char)ig;
+            image[3*nx*(ny-1-j)+3*i+2] = (char)ib;
             cout<<ir<<" "<<ig<<" "<<ib<<"\n";
         }
     }
+    encodeOneStep("Chapter3.png", image, nx, ny);
 }
+
