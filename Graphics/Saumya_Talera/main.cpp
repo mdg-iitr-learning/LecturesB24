ff6a99dfe1fb1284484b720dd74361a65b3a1400
Saumya <saumya_t@cs.iitr.ac.in>
2021-04-29 21:21:19 +0530

    CHAPTER 4
    - Implemented Chapter 4

--- Graphics/Saumya_Talera/main.cpp
+++ Graphics/Saumya_Talera/main.cpp
@@ -13,7 +13,18 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
   if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin()-center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0*dot(oc, r.direction());
+    float c = dot(oc,oc)-radius*radius;
+    float discriminant = b*b-4*a*c;
+    return (discriminant>0);
+}
+
 vec3 color(const ray& r) {
+    if(hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y()+1.0);
     return (1-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
@@ -44,6 +55,6 @@ int main() {
             cout<<ir<<" "<<ig<<" "<<ib<<"\n";
         }
     }
-    encodeOneStep("Chapter3.png", image, nx, ny);
+    encodeOneStep("Chapter4.png", image, nx, ny);
 }
 
